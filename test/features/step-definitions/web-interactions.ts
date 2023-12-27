b08e3ea2d879f09adf35e0b0387f64654743330c
JeanColonia <engshka123@gmail.com>
2023-12-27 15:18:09 -0500

    dropdown

--- test/features/step-definitions/web-interactions.ts
+++ test/features/step-definitions/web-interactions.ts
@@ -4,11 +4,19 @@ import chai from 'chai';
 
 Given(/^a Web is opened/, async function () {
 
- await browser.url("https://the-internet.herokuapp.com/");
+ await browser.url("https://the-internet.herokuapp.com/dropdown");
 });
 
 When(/^perform web Interactions/, async function () {
 
- await browser.debug();
+ let ele = await $("//select/option[@selected='selected']");
+
+ let val = await ele.getText();
+
+ chai.expect(val).to.equal("Please select an option");
+
+
+
+
 });
 
