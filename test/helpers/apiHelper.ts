b36faf17c9ccc492a0aed8e654c605967d7a20d1
JeanColonia <engshka123@gmail.com>
2024-01-12 16:10:12 -0500

    POST method with SuperTest

--- test/helpers/apiHelper.ts
+++ test/helpers/apiHelper.ts
@@ -3,6 +3,10 @@ import reporter from './reporter.ts';
 
 
 
+let payload = {
+ name: "morpheus",
+ job: "leader"
+}
 
 async function GET(testid: string, baseUrl: string, endpoint: string, authToken: string, queryParam: Object) {
 
@@ -24,4 +28,24 @@ async function GET(testid: string, baseUrl: string, endpoint: string, authToken:
 }
 
 
-export default { GET }
+
+async function POST(testid: string, baseUrl: string, endpoint: string, authToken: string, payload: object) {
+ //validate baseUrl and endpoint 
+ if (!baseUrl || !endpoint) throw Error(`One of given values BaseUrl: ${baseUrl}, endpoint: ${endpoint} is not valid`);
+ baseUrl = baseUrl.trim()
+ endpoint = endpoint.trim()
+
+
+ reporter.addStep(testid, "info", `Making a POST requesto to ${baseUrl}${endpoint}`)
+ //use method (post ) with base url - endpoint 
+ try {
+  let res = await request(baseUrl).post(endpoint).auth(authToken, { type: "bearer" }).set("Content-Type", "application/json").set("Accept", "application/json").send(payload);
+
+  console.log(`Result of request: ${JSON.stringify(res.body)}`)
+ } catch (error) {
+  throw error;
+ }
+
+}
+
+export default { GET, POST }
